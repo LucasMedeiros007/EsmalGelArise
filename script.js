e1955e2d5c083f4f4447c6cde21901da93b6991f
Lucas Fiaes <lm3355899@gmail.com>
2025-04-12 18:09:16 -0400

    Antes do Menu

---
+++ script.js
@@ -0,0 +1,13 @@
+
+// Seleciona o carrossel
+const carrosselTrack = document.querySelector('.carrossel-track');
+
+// Quando o usuário tocar (finger down), pausa a animação
+carrosselTrack.addEventListener('touchstart', () => {
+carrosselTrack.style.animationPlayState = 'paused';
+});
+
+// Quando o usuário parar de tocar (finger up), retoma a animação
+carrosselTrack.addEventListener('touchend', () => {
+carrosselTrack.style.animationPlayState = 'running';
+});
