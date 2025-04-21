329d0ef5514c339dfc4076dd71f5e401375ae456
Lucas Fiaes <lm3355899@gmail.com>
2025-04-21 16:50:01 -0400

    barra adicionada

---
+++ navBar.js
@@ -0,0 +1,19 @@
+function abrirNav(){
+    document.getElementById("myNav").style.width = '100%';
+    document.getElementById("myNav").style.height = '100%';
+}
+
+function fecharNav(){
+    document.getElementById("myNav").style.width = '0';
+    document.getElementById("myNav").style.height = '0';
+}
+
+const botao = document.getElementById("botao");
+
+botao.addEventListener('touchstart', () => {
+botao.style.padding = '12px 24px';
+});
+
+botao.addEventListener('touchend', () => {
+    botao.style.padding = '10px 20px';
+    });
