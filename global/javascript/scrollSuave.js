f3c0d8bfa9068f917d97fadca61c484c2027c03b
Lucas Fiaes <lm3355899@gmail.com>
2025-07-30 22:59:58 -0400

    duracao, o que aprender e indicado alterado

--- global/javascript/scrollSuave.js
+++ global/javascript/scrollSuave.js
@@ -7,7 +7,7 @@ function initScrollSuave() {
     const section = document.querySelector(href);
     section.scrollIntoView({
       behavior: 'smooth',
-      block: 'start',
+      block: 'center',
     });
 
     // forma alternativa
