844c6d52ac8ad27b6f2b4091d2b5e1c12634706b
LucasMedeiros007 <lm3355899@gmail.com>
2025-10-02 12:48:12 -0400

    teste

--- global/javascript/slide.js
+++ global/javascript/slide.js
@@ -9,152 +9,152 @@ if (window.innerWidth > 50000) {
   slide.style.paddingRight = '40px';
   slide.style.paddingLeft = '40px';
 }
-function teste() {
-  if (window.innerWidth <= 500) {
-    const slide = document.querySelector('.servicos ul');
-    const wrapper = document.querySelector('.servicos');
-    const dist = { finalPosition: 0, startX: 0, movement: 0 };
-    let contador = 0;
-    let indice = {
-      prev: 0,
-      active: 1,
-      next: 1,
-    };
+// function teste() {
+//   if (window.innerWidth <= 500) {
+//     const slide = document.querySelector('.servicos ul');
+//     const wrapper = document.querySelector('.servicos');
+//     const dist = { finalPosition: 0, startX: 0, movement: 0 };
+//     let contador = 0;
+//     let indice = {
+//       prev: 0,
+//       active: 1,
+//       next: 1,
+//     };
 
-    let tempo5;
-    function onStart(event) {
-      transition(false);
-      let typeMove;
-      if (event.type === 'mousedown') {
-        event.preventDefault();
-        dist.startX = event.clientX;
-        typeMove = 'mousemove';
-      } else {
-        dist.startX = event.changedTouches[0].clientX;
-        typeMove = 'touchmove';
-      }
-      wrapper.addEventListener(typeMove, onMove);
-    }
+//     let tempo5;
+//     function onStart(event) {
+//       transition(false);
+//       let typeMove;
+//       if (event.type === 'mousedown') {
+//         event.preventDefault();
+//         dist.startX = event.clientX;
+//         typeMove = 'mousemove';
+//       } else {
+//         dist.startX = event.changedTouches[0].clientX;
+//         typeMove = 'touchmove';
+//       }
+//       wrapper.addEventListener(typeMove, onMove);
+//     }
 
-    function onMove(event) {
-      const pointerPosition =
-        event.type === 'mousemove'
-          ? event.clientX
-          : event.changedTouches[0].clientX;
-      const posicaoFinal = updatePosition(pointerPosition);
-      moveSlide(posicaoFinal);
-    }
+//     function onMove(event) {
+//       const pointerPosition =
+//         event.type === 'mousemove'
+//           ? event.clientX
+//           : event.changedTouches[0].clientX;
+//       const posicaoFinal = updatePosition(pointerPosition);
+//       moveSlide(posicaoFinal);
+//     }
 
-    function onEnd(event) {
-      const moveType = event.type === 'mouseup' ? 'mousemove' : 'touchmove';
-      wrapper.removeEventListener(moveType, onMove);
-      dist.finalPosition = dist.movePosition;
-      transition(true);
-      changeSlideOnEnd();
+//     function onEnd(event) {
+//       const moveType = event.type === 'mouseup' ? 'mousemove' : 'touchmove';
+//       wrapper.removeEventListener(moveType, onMove);
+//       dist.finalPosition = dist.movePosition;
+//       transition(true);
+//       changeSlideOnEnd();
 
-      contador = indice.active;
-    }
+//       contador = indice.active;
+//     }
 
-    function updatePosition(clientX) {
-      dist.movement = dist.startX - clientX;
-      return dist.finalPosition - dist.movement;
-    }
+//     function updatePosition(clientX) {
+//       dist.movement = dist.startX - clientX;
+//       return dist.finalPosition - dist.movement;
+//     }
 
-    function moveSlide(distX) {
-      slide.style.transform = `translate3d(${distX}px,0,0)`;
-      dist.movePosition = distX;
-    }
+//     function moveSlide(distX) {
+//       slide.style.transform = `translate3d(${distX}px,0,0)`;
+//       dist.movePosition = distX;
+//     }
 
-    //slidesconfig
-    function changeSlideOnEnd() {
-      if (dist.movement > 30 && indice.next !== undefined) {
-        activeNextSlide();
-      } else if (dist.movement < -30 && indice.prev !== undefined) {
-        activePrevSlide();
-      } else {
-        changeSlide(indice.active);
-      }
-    }
+//     //slidesconfig
+//     function changeSlideOnEnd() {
+//       if (dist.movement > 30 && indice.next !== undefined) {
+//         activeNextSlide();
+//       } else if (dist.movement < -30 && indice.prev !== undefined) {
+//         activePrevSlide();
+//       } else {
+//         changeSlide(indice.active);
+//       }
+//     }
 
-    function slidePosition(slide) {
-      const margin = (wrapper.offsetWidth - slide.offsetWidth) / 2;
-      return -(slide.offsetLeft - margin);
-    }
+//     function slidePosition(slide) {
+//       const margin = (wrapper.offsetWidth - slide.offsetWidth) / 2;
+//       return -(slide.offsetLeft - margin);
+//     }
 
-    let slideArray;
-    function slidesConfig() {
-      slideArray = [...slide.children].map((element) => {
-        const position = slidePosition(element);
-        return {
-          position,
-          element,
-        };
-      });
-    }
+//     let slideArray;
+//     function slidesConfig() {
+//       slideArray = [...slide.children].map((element) => {
+//         const position = slidePosition(element);
+//         return {
+//           position,
+//           element,
+//         };
+//       });
+//     }
 
-    function changeSlide(index) {
-      moveSlide(slideArray[index].position);
-      slideIndexNav(index);
-      dist.finalPosition = slideArray[index].position;
-    }
+//     function changeSlide(index) {
+//       moveSlide(slideArray[index].position);
+//       slideIndexNav(index);
+//       dist.finalPosition = slideArray[index].position;
+//     }
 
-    function slideIndexNav(index) {
-      const last = slideArray.length - 1;
-      indice = {
-        prev: index ? index - 1 : undefined,
-        active: index,
-        next: index === last ? undefined : index + 1,
-      };
-    }
+//     function slideIndexNav(index) {
+//       const last = slideArray.length - 1;
+//       indice = {
+//         prev: index ? index - 1 : undefined,
+//         active: index,
+//         next: index === last ? undefined : index + 1,
+//       };
+//     }
 
-    function activePrevSlide() {
-      if (indice.prev !== undefined) {
-        changeSlide(indice.prev);
-      }
-    }
-    function activeNextSlide() {
-      if (indice.next !== undefined) {
-        changeSlide(indice.next);
-      }
-    }
+//     function activePrevSlide() {
+//       if (indice.prev !== undefined) {
+//         changeSlide(indice.prev);
+//       }
+//     }
+//     function activeNextSlide() {
+//       if (indice.next !== undefined) {
+//         changeSlide(indice.next);
+//       }
+//     }
 
-    function transition(ativo, trans = '.3s') {
-      slide.style.transition = ativo ? 'transform ' + trans : '';
-    }
+//     function transition(ativo, trans = '.3s') {
+//       slide.style.transition = ativo ? 'transform ' + trans : '';
+//     }
 
-    function onResize() {
-      setTimeout(() => {
-        slidesConfig();
-        changeSlide(indice.active);
-      }, 100);
-    }
+//     function onResize() {
+//       setTimeout(() => {
+//         slidesConfig();
+//         changeSlide(indice.active);
+//       }, 100);
+//     }
 
-    window.addEventListener('resize', onResize);
+//     window.addEventListener('resize', onResize);
 
-    slidesConfig();
+//     slidesConfig();
 
-    wrapper.addEventListener('mousedown', onStart);
-    wrapper.addEventListener('touchstart', onStart);
-    wrapper.addEventListener('mouseup', onEnd);
-    wrapper.addEventListener('touchend', onEnd);
+//     wrapper.addEventListener('mousedown', onStart);
+//     wrapper.addEventListener('touchstart', onStart);
+//     wrapper.addEventListener('mouseup', onEnd);
+//     wrapper.addEventListener('touchend', onEnd);
 
-    //   let intervaloSemTocar;
-    //   function iniciarIntervalo() {
-    //     intervaloSemTocar = setInterval(intervalo, 2550);
-    //   }
-    // iniciarIntervalo();
-    //   function intervalo() {
-    //     contador++;
-    //     if (contador <= slideArray.length - 1) {
-    //       transition(true, '0.7s');
-    //       changeSlide((indice.prev = contador));
-    //     } else {
-    //       transition(true, '1.3s');
-    //       changeSlide((indice.active = 0));
-    //       contador = 0;
-    //     }
-    //   }
-  }
-}
+//     //   let intervaloSemTocar;
+//     //   function iniciarIntervalo() {
+//     //     intervaloSemTocar = setInterval(intervalo, 2550);
+//     //   }
+//     // iniciarIntervalo();
+//     //   function intervalo() {
+//     //     contador++;
+//     //     if (contador <= slideArray.length - 1) {
+//     //       transition(true, '0.7s');
+//     //       changeSlide((indice.prev = contador));
+//     //     } else {
+//     //       transition(true, '1.3s');
+//     //       changeSlide((indice.active = 0));
+//     //       contador = 0;
+//     //     }
+//     //   }
+//   }
+// }
 
-window.addEventListener('resize', teste);
+// window.addEventListener('resize', teste);
